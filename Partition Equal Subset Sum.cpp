13f84a0158ecf8963c3713ce1b3afb99aea3b4b5
Anamika Kushwaha <aiml23058@glbitm.ac.in>
2026-04-14 01:29:10 +0530

    Partition Equal Subset Sum.cpp
    GFG LINK:- https://www.geeksforgeeks.org/problems/subset-sum-problem2014/1

---
+++ Partition Equal Subset Sum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+  public:
+    bool solve(vector<int>& arr, int n, int sum,vector<vector<int>> & memo){
+        if(sum==0) return 1;
+        if(n<=0) return 0;
+        if(memo[n][sum] != -1) {
+            return memo[n][sum];
+        }
+        
+        if(arr[n-1]>sum){
+            return memo[n][sum]= solve(arr,n-1,sum,memo);
+        }
+        else{
+            return memo[n][sum]= solve(arr,n-1,sum,memo)||
+                                 solve(arr,n-1,sum-arr[n-1],memo);
+        }
+    }
+    
+    bool isSubsetSum(vector<int>& arr, int sum) {
+        int n= arr.size();
+        vector<vector<int>> memo(n + 1, vector<int>(sum + 1, -1));
+        return solve(arr,n,sum,memo);
+        
+    }
+    bool equalPartition(vector<int>& arr) {
+        int sum = accumulate(arr.begin(),arr.end(),0);
+        if (sum%2 != 0) return false;
+        vector<vector<int>> memo(arr.size(),vector<int>(sum+1,-1));
+        return isSubsetSum(arr,sum/2);
+            
+    }
+};
